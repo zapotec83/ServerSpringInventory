be98564c983297674caccf6a36dfa571ed1bce92
jorge <jorgerider83@gmail.com>
2015-02-13 19:15:08 +0100

    Changes for ApplicationContext

--- src/main/java/com/companyname/springapp/impl/ProductManagerImpl.java
+++ src/main/java/com/companyname/springapp/impl/ProductManagerImpl.java
@@ -2,26 +2,32 @@ package com.companyname.springapp.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.companyname.springapp.model.Product;
+import com.companyname.springapp.repository.ProductDAO;
 import com.companyname.springapp.service.ProductManager;
 
 public class ProductManagerImpl implements ProductManager {
 
-	private List<Product> products;
-	
+    @Autowired
+    private ProductDAO productDao;
+
     public List<Product> getProducts() {
-        return products;       
-    }
-    
-    public void setProducts(List<Product> products) {
-        this.products = products;
+    	return productDao.getProductList();
     }
 
+    public void setProductDao(ProductDAO productDao) {
+        this.productDao = productDao;
+    }
+    
     public void increasePrice(int percentage) {
+    	List<Product> products = productDao.getProductList();
         if (products != null) {
             for (Product product : products) {
                 double newPrice = product.getPrice().doubleValue() * (100 + percentage)/100;
                 product.setPrice(newPrice);
+                productDao.saveProduct(product);
             }
         }  
     }
